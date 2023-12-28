66c4b94f51c25fb74990d1b2e2a8d527902452bb
pl <0x00.pl@gmail.com>
2023-12-28 12:42:14 +0800

    update script

--- app.sh
+++ app.sh
@@ -2,8 +2,10 @@ echo "start logging..."
 
 echo "[pl][debug] connecting: ${USER}:${PASS}"
 
-echo -e "${PASS}" | openconnect ${SERVER} --user="${USER}" --passwd-on-stdin -b
+echo -e "${PASS}" | openconnect ${SERVER} --user="${USER}" --passwd-on-stdin --reconnect-timeout 10 -b
 sleep 10
+
+ping z.cn -i 120 &  # keepalive
 
 ss-server &
 ss-local -c /etc/shadowsocks-libev/local.json -v
